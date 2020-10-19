8d37ee37a30df4bdd00ccb4ddff97035786b46da
Aditi Varade <aditi_varade@persistent.co.in>
2020-10-19 13:57:30 +0530

    Add println to test git with jenkins after commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("Testing SCM Git Integration With Jenkins!"); 
+        System.out.println("Testing SCM Git Integration With Jenkins after commiting a change in GitHub!"); 
     }
 }
