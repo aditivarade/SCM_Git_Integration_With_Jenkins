413396fe25633fc7b6d6060ebf9ef01637075a71
Aditi Varade <aditi_varade@persistent.co.in>
2020-10-20 10:55:51 +0530

    Added println to test scm git after updation

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("Testing SCM Git Integration With Jenkins!"); 
+        System.out.println("Testing SCM Git Intergartion with Jenkins after commit and push in GitHub!!");
     }
 }
